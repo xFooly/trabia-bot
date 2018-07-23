07f9dc778aa568b3e493d17ec66effc228e07aa3
xFooly <andrea.gallidabino@gmail.com>
2018-07-23 11:03:45 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+	database: 'mongodb://localhost:27017/trabia', // database link
+
+	prefix: '-', // Your command prefix
+
+	token: '', // Your discord bot token here
+	allowedChannels: [] // List of channel IDs here
+}
+
+module.exports = config
